84d114e367b3b499445583a7ec81a0f5ba99dee0
Ayomikun <elijahsholademi@gmail.com>
2022-02-10 10:06:09 +0100

    update

---
+++ widgets/dashboard/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card-container"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.card-container {
+  height: 176px;
+}
+</style>
---
+++ widgets/dashboard/DInfoCards.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-10/12 mx-auto grid grid-cols-3 gap-6"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
